bb4145e03671392c007635ad04e5416b017a08b1
Vacannot <44813993+Vacannot@users.noreply.github.com>
2021-12-22 01:54:41 +0100

    bgnbnvm

--- javascript/toDo.js
+++ javascript/toDo.js
@@ -13,7 +13,7 @@ for (a = 0; a < theToDos.length; a++) {
 const close = document.getElementsByClassName('close')
 var i;
 for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
+    close[i].onclick = function() {
         const closeParent = this.parentElement;
         closeParent.remove();
     }
@@ -21,35 +21,43 @@ for (i = 0; i < close.length; i++) {
 
 /* The "Add" button functionality */
 const add = document.querySelector('.btnAddToDo')
-add.onclick = function () {
-    const li = document.createElement("li");
-    const inputValue = document.getElementById("toDoTextArea").value;
-    const txt = document.createTextNode(inputValue);
-    li.appendChild(txt);
-    if (inputValue === '') {
-        alert("You can't make an empty \"to do\"");
-    } else {
-        document.getElementById("toDoItemsUL").appendChild(li);
-    }
-    document.getElementById("toDoTextArea").value = "";
+add.onclick = function() {
+        const li = document.createElement("li");
+        const inputValue = document.getElementById("toDoTextArea").value;
+        const txt = document.createTextNode(inputValue);
+        li.appendChild(txt);
+        if (inputValue === '') {
+            alert("You can't make an empty \"to do\"");
+        } else {
+            document.getElementById("toDoItemsUL").appendChild(li);
+            todoCounter()
+        }
+        document.getElementById("toDoTextArea").value = "";
 
-    const span = document.createElement("span");
-    const symbol = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(symbol);
-    li.appendChild(span);
+        const span = document.createElement("span");
+        const symbol = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(symbol);
+        li.appendChild(span);
 
-    for (a = 0; a < close.length; a++) {
-        close[a].onclick = function () {
-            const closeParent = this.parentElement;
-            closeParent.remove();
+        for (a = 0; a < close.length; a++) {
+            close[a].onclick = function() {
+                const closeParent = this.parentElement;
+                closeParent.remove();
+            }
         }
     }
-}
-/* Check function */
+    /* Check function */
 const checkList = document.querySelector('.toDoUL');
 checkList.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
 }, false)
+
+function todoCounter() {
+    let todayCell = document.getElementsByClassName("today")
+    let listSize = document.querySelectorAll("li").length
+    console.log(listSize)
+    todayCell.innerHTML = listSize
+}
